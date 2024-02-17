4bb92b78d871e81516cef3dd8b32d5ced7daa3eb
sheerazmehboob <sherazmehboob14@gmail.com>
2024-02-18 02:50:27 +0500

    Redux-Thunk for Async Actions

--- redux-toolkit/app/store.js
+++ redux-toolkit/app/store.js
@@ -2,6 +2,7 @@ const configureStore = require('@reduxjs/toolkit').configureStore
 const reduxLogger = require('redux-logger')
 const cakeReducer = require('../features/cake/cakeSlice')
 const iceccreamReducer = require('../features/icecream/icecreamSlice')
+const userReducer = require('../features/user/userSlice')
 
 const logger = reduxLogger.createLogger()
 
@@ -9,6 +10,7 @@ const store = configureStore({
     reducer: {
         cake: cakeReducer,
         icecream: iceccreamReducer,
+        user: userReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
---
+++ redux-toolkit/features/user/userSlice.js
@@ -0,0 +1,40 @@
+const createSlice = require("@reduxjs/toolkit").createSlice;
+const createAsyncThunk = require("@reduxjs/toolkit").createAsyncThunk;
+const axios = require("axios");
+
+const initialState = {
+  loading: false,
+  users: [],
+  error: "",
+};
+
+// Invoking asyncThunk Function. This function accepts 2 parameters -> Action Name -> callback function that create the payload
+// This ayncThunk function automatically generates pending, fulfilled, and rejected action types
+const fetchUsers = createAsyncThunk("user/fetchUsers", () => {
+  return axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.data.map((user) => user.id));
+});
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.loading = false;
+      state.users = action.paylaod;
+      state.error = "";
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.loading = false;
+      state.users = [];
+      state.error = action.error.message;
+    });
+  },
+});
+
+module.exports = userSlice.reducer;
+module.exports.fetchUsers = fetchUsers;
